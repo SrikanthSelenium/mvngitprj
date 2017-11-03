67b49d0d193c86d6347118ce58f4a178d4867e17
srikanth <srikanth.chincholi@gmail.com>
2017-11-04 00:45:26 +0530

    adding all files

---
+++ src/test/java/mvngittest/SimpleClass.java
@@ -0,0 +1,13 @@
+package mvngittest;
+
+import org.testng.annotations.Test;
+
+public class SimpleClass {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Just run this and show it on console");
+	}
+
+}
